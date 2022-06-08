afdcd083f8057e0b40385db441fcafa596f60cd2
Jeckson <jeckson.silva@yahoo.com.br>
2022-06-08 18:59:50 -0300

    index

---
+++ main.js/script.js
@@ -0,0 +1,15 @@
+/*Script open and close configuration*/
+
+function openNav(){
+    document.getElementById("menuOculto").style.width="250px";
+    document.getElementById("principal").style.marginleft="250px";
+}
+
+function closeNav(){
+    document.getElementById("menuOculto").style.width="0";
+    document.getElementById("principal").style.marginleft="0";
+
+
+}/*End configuration*/
+
+
